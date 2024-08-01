228572d24005cabb2a640e9adedc42b52dbc5d13
Jisu <hellojs242@gmail.com>
2024-08-01 20:51:06 +0900

    :hammer: chore: Connect postgres DB to server using env file

--- recruitment-app/src/app.module.ts
+++ recruitment-app/src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        type: 'postgres',
+        host: process.env.POSTGRES_HOST || 'localhost',
+        port: parseInt(process.env.POSTGRES_PORT || '5432'),
+        username: process.env.POSTGRES_USER,
+        password: String(process.env.POSTGRES_PASSWORD),
+        database: process.env.POSTGRES_DB,
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

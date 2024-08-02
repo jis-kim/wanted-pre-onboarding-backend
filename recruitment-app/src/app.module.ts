a72105a8a089d95774257b3be88d88f0b1c22145
Jisu <hellojs242@gmail.com>
2024-08-02 23:53:09 +0900

    :recycle: refacroe: Refactor TypeORM connection to use URL

--- recruitment-app/src/app.module.ts
+++ recruitment-app/src/app.module.ts
@@ -10,11 +10,12 @@ import { ConfigModule } from '@nestjs/config';
     TypeOrmModule.forRootAsync({
       useFactory: () => ({
         type: 'postgres',
-        host: process.env.POSTGRES_HOST || 'localhost',
-        port: parseInt(process.env.POSTGRES_PORT || '5432'),
-        username: process.env.POSTGRES_USER,
-        password: String(process.env.POSTGRES_PASSWORD),
-        database: process.env.POSTGRES_DB,
+        //host: process.env.POSTGRES_HOST || 'localhost',
+        //port: parseInt(process.env.POSTGRES_PORT || '5432'),
+        //username: process.env.POSTGRES_USER,
+        //password: process.env.POSTGRES_PASSWORD,
+        //database: process.env.POSTGRES_DB,
+        url: process.env.POSTGRES_URL,
       }),
     }),
   ],

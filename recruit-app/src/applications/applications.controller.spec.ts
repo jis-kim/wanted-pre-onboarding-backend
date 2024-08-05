ab87d13e0192821050dfd7e03aeaa143d5f32936
Jisu <hellojs242@gmail.com>
2024-08-05 22:41:33 +0900

    :test_tube: test: Test applications module

--- recruit-app/src/applications/applications.controller.spec.ts
+++ recruit-app/src/applications/applications.controller.spec.ts
@@ -1,20 +1,75 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ApplicationsController } from './applications.controller';
 import { ApplicationsService } from './applications.service';
+import { CreateApplicationDto } from './dto/create-application.dto';
+import { BadRequestException, ValidationPipe } from '@nestjs/common';
 
 describe('ApplicationsController', () => {
   let controller: ApplicationsController;
+  let service: ApplicationsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ApplicationsController],
-      providers: [ApplicationsService],
+      providers: [
+        {
+          provide: ApplicationsService,
+          useValue: {
+            create: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     controller = module.get<ApplicationsController>(ApplicationsController);
+    service = module.get<ApplicationsService>(ApplicationsService);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  describe('create', () => {
+    it('요청 성공 시 201 Created. Location 헤더를 함께 보낸다.', async () => {
+      const dto: CreateApplicationDto = {
+        jobId: '123456789012345678901', // 21자
+        userId: '123456789012345678901',
+        title: 'Test Application',
+        content: 'This is a test application content.',
+      };
+      const mockApplicationId = 'mockApplicationId123';
+      const mockResponse = {
+        setHeader: jest.fn(),
+        status: jest.fn().mockReturnThis(),
+        send: jest.fn(),
+      };
+
+      jest.spyOn(service, 'create').mockResolvedValue(mockApplicationId);
+
+      await controller.create(dto, mockResponse as any);
+
+      expect(service.create).toHaveBeenCalledWith(dto);
+      expect(mockResponse.setHeader).toHaveBeenCalledWith(
+        'Location',
+        `/applications/${mockApplicationId}`,
+      );
+      expect(mockResponse.status).toHaveBeenCalledWith(201);
+      expect(mockResponse.send).toHaveBeenCalledWith({
+        message: 'Application created.',
+        applicationId: mockApplicationId,
+      });
+    });
+
+    it('잘못된 request 형식일 경우 400 Bad Request Error.', async () => {
+      const invalidDto = {
+        jobId: 'invalid', // Too short
+        userId: '123456789012345678901',
+        content: 'Valid content',
+      };
+
+      const validator = new ValidationPipe();
+      await expect(
+        validator.transform(invalidDto, {
+          type: 'body',
+          metatype: CreateApplicationDto,
+        }),
+      ).rejects.toThrow(BadRequestException);
+    });
   });
 });
--- recruit-app/src/applications/applications.service.spec.ts
+++ recruit-app/src/applications/applications.service.spec.ts
@@ -1,18 +1,86 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ApplicationsService } from './applications.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Application } from '../entities';
+import { InsertResult, Repository } from 'typeorm';
+import { CreateApplicationDto } from './dto/create-application.dto';
+import { ConflictException, NotFoundException } from '@nestjs/common';
 
 describe('ApplicationsService', () => {
   let service: ApplicationsService;
+  let repository: Repository<Application>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ApplicationsService],
+      providers: [
+        ApplicationsService,
+        {
+          provide: getRepositoryToken(Application),
+          useValue: {
+            findOne: jest.fn(),
+            insert: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<ApplicationsService>(ApplicationsService);
+    repository = module.get(getRepositoryToken(Application));
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('create', () => {
+    const createApplicationDto: CreateApplicationDto = {
+      jobId: 'job123',
+      userId: 'user123',
+      title: 'Application Title',
+      content: 'Application Content',
+    };
+
+    it('application 생성 성공', async () => {
+      jest.spyOn(repository, 'findOne').mockResolvedValue(null);
+      jest.spyOn(repository, 'insert').mockResolvedValue(new InsertResult());
+
+      const result = await service.create(createApplicationDto);
+
+      expect(result).toBeDefined();
+      expect(typeof result).toBe('string');
+      expect(repository.findOne).toHaveBeenCalledWith({
+        where: {
+          jobId: createApplicationDto.jobId,
+          userId: createApplicationDto.userId,
+        },
+      });
+      expect(repository.insert).toHaveBeenCalled();
+    });
+
+    it('이미 지원한 채용공고인 경우 ConflictException', async () => {
+      jest.spyOn(repository, 'findOne').mockResolvedValue({} as Application);
+
+      await expect(service.create(createApplicationDto)).rejects.toThrow(
+        ConflictException,
+      );
+      expect(repository.findOne).toHaveBeenCalledWith({
+        where: {
+          jobId: createApplicationDto.jobId,
+          userId: createApplicationDto.userId,
+        },
+      });
+      expect(repository.insert).not.toHaveBeenCalled();
+    });
+
+    it('jobId 가 유효하지 않은 경우 NotFoundException', async () => {
+      jest.spyOn(repository, 'findOne').mockResolvedValue(null);
+      jest.spyOn(repository, 'insert').mockRejectedValue({ code: '23503' });
+
+      await expect(service.create(createApplicationDto)).rejects.toThrow(
+        NotFoundException,
+      );
+      expect(repository.findOne).toHaveBeenCalled();
+      expect(repository.insert).toHaveBeenCalled();
+    });
   });
 });
